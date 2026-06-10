0d887c991fcc5c46d5092936d94134adc910749e
Didarul Alam <didarulalamrafi1312@gmail.com>
2026-06-10 12:45:09 +0600

    social

--- src/app/components/homepage/news/Social.jsx
+++ src/app/components/homepage/news/Social.jsx
@@ -1,12 +1,20 @@
+"use client"
+import { authClient } from "@/lib/auth-client";
 import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const Social = () => {
+  const handleLoginWithGoogle = async ()=>{
+    const data = await authClient.signIn.social({
+    provider: "google",
+  });
+  console.log(data, "data")
+  }
   return (
     <div className="text-center my-2">
       <h3>Login with</h3>
       <div className="flex flex-col gap-2">
-        <button className="btn">
+        <button className="btn" onClick={handleLoginWithGoogle}>
           {" "}
           <FaGoogle />
           Login with Google
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,5 @@
-import dns from "node:dns";
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// import dns from "node:dns";
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
@@ -15,4 +15,10 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+        google: { 
+            clientId:process.env.GOOGLE_CLIENT_ID, 
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET  
+        }, 
+      }
 });
